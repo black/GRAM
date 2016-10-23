20d6d999a944e8e082b50e53654ee7497279cc9e
Black <ixd.abhi@gmail.com>
2016-10-24 01:45:01 +0530

    Fixed delete functionality

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace CatView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.fitToScreen = new System.Windows.Forms.Button();
+            this.prevButton = new System.Windows.Forms.Button();
+            this.setAsDesktop = new System.Windows.Forms.Button();
+            this.tableLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayout
+            // 
+            this.tableLayout.AutoSize = true;
+            this.tableLayout.BackColor = System.Drawing.Color.White;
+            this.tableLayout.ColumnCount = 1;
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayout.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayout.Controls.Add(this.panel1, 0, 1);
+            this.tableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayout.Location = new System.Drawing.Point(0, 0);
+            this.tableLayout.Name = "tableLayout";
+            this.tableLayout.RowCount = 2;
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 56F));
+            this.tableLayout.Size = new System.Drawing.Size(807, 457);
+            this.tableLayout.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 395);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.openFile);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.Controls.Add(this.deleteButton);
+            this.panel1.Controls.Add(this.nextButton);
+            this.panel1.Controls.Add(this.fitToScreen);
+            this.panel1.Controls.Add(this.prevButton);
+            this.panel1.Controls.Add(this.setAsDesktop);
+            this.panel1.Location = new System.Drawing.Point(286, 406);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(235, 46);
+            this.panel1.TabIndex = 1;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("deleteButton.BackgroundImage")));
+            this.deleteButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.deleteButton.FlatAppearance.BorderSize = 0;
+            this.deleteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteButton.Location = new System.Drawing.Point(191, 4);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(40, 40);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = " ";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nextButton.BackgroundImage")));
+            this.nextButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.nextButton.FlatAppearance.BorderSize = 0;
+            this.nextButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nextButton.Location = new System.Drawing.Point(144, 4);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(40, 40);
+            this.nextButton.TabIndex = 3;
+            this.nextButton.Text = " ";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // fitToScreen
+            // 
+            this.fitToScreen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("fitToScreen.BackgroundImage")));
+            this.fitToScreen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.fitToScreen.FlatAppearance.BorderSize = 0;
+            this.fitToScreen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.fitToScreen.Location = new System.Drawing.Point(97, 4);
+            this.fitToScreen.Name = "fitToScreen";
+            this.fitToScreen.Size = new System.Drawing.Size(40, 40);
+            this.fitToScreen.TabIndex = 2;
+            this.fitToScreen.Text = " ";
+            this.fitToScreen.UseVisualStyleBackColor = true;
+            this.fitToScreen.Click += new System.EventHandler(this.fitToScreen_Click);
+            // 
+            // prevButton
+            // 
+            this.prevButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("prevButton.BackgroundImage")));
+            this.prevButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.prevButton.FlatAppearance.BorderSize = 0;
+            this.prevButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.prevButton.Location = new System.Drawing.Point(50, 4);
+            this.prevButton.Name = "prevButton";
+            this.prevButton.Size = new System.Drawing.Size(40, 40);
+            this.prevButton.TabIndex = 1;
+            this.prevButton.Text = " ";
+            this.prevButton.UseVisualStyleBackColor = true;
+            this.prevButton.Click += new System.EventHandler(this.prevButton_Click);
+            // 
+            // setAsDesktop
+            // 
+            this.setAsDesktop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("setAsDesktop.BackgroundImage")));
+            this.setAsDesktop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.setAsDesktop.FlatAppearance.BorderSize = 0;
+            this.setAsDesktop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.setAsDesktop.Location = new System.Drawing.Point(3, 3);
+            this.setAsDesktop.Name = "setAsDesktop";
+            this.setAsDesktop.Size = new System.Drawing.Size(40, 40);
+            this.setAsDesktop.TabIndex = 0;
+            this.setAsDesktop.Text = " ";
+            this.setAsDesktop.UseVisualStyleBackColor = true;
+            this.setAsDesktop.Click += new System.EventHandler(this.setAsDesktop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 457);
+            this.Controls.Add(this.tableLayout);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(320, 240);
+            this.Name = "Form1";
+            this.Text = "G R A M";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayout;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button setAsDesktop;
+        private System.Windows.Forms.Button fitToScreen;
+        private System.Windows.Forms.Button prevButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button nextButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatView
+{
+    public partial class Form1 : Form
+    {
+
+        List<String> allImages = new List<String>();
+        int k = 0;
+        String filepath = null;
+
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        public Form1(String[] file)
+        {
+            InitializeComponent(); 
+            if (file.Length != 0)
+                filepath = file[0];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (filepath != null)
+            {
+                String pathDirectory = Path.GetDirectoryName(filepath);
+                allImages = GetImagesPath(pathDirectory);
+                k = allImages.IndexOf(filepath);
+                setFileName(pathDirectory);
+                fitImage(filepath);
+            }
+        }
+
+        private void openFile(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            // ofd.Filter = "jpg (*.jpg)|*.jpg|bmp (*bmp)|*.bmp|png (*.png)|*png|gif (*.gif)|*.gif";
+
+            if (ofd.ShowDialog() == DialogResult.OK && ofd.FileName.Length > 0)
+            {
+                String path = ofd.FileName;
+                String pathDirectory = Path.GetDirectoryName(path);
+                allImages = GetImagesPath(pathDirectory);
+                setFileName(ofd.FileName);
+                k = allImages.IndexOf(path);
+                fitImage(path);
+                //  Dispose();
+            }
+        }
+
+        public List<String> GetImagesPath(String folderName)
+        {
+            List<String> imagesList = new List<String>();
+            var extensionList = new List<string> { "*.jpg", "*.gif", "*.png", "*.bmp", "*.jpeg" };
+            foreach (String fileExtension in extensionList)
+            {
+                foreach (String file in Directory.GetFiles(folderName, fileExtension))  /* to get all files  from sub directories:   foreach (String file in Directory.GetFiles(folderName, fileExtension, SearchOption.AllDirectories)) */
+
+                {
+                    imagesList.Add(file);
+                }
+            }
+
+            return imagesList;
+        }
+
+        private void fitImage(String path)
+        {
+            pictureBox1.Image = Image.FromFile(path);
+            int imgWidth = pictureBox1.Image.Size.Width;
+            int imgHeight = pictureBox1.Image.Size.Height;
+            int pBoxWidth = pictureBox1.Size.Width;
+            int pBoxHeight = pictureBox1.Size.Height;
+
+            if (imgWidth > pBoxWidth || imgHeight > pBoxHeight)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            }
+        }
+
+
+        private void prevButton_Click(object sender, EventArgs e)
+        {
+            goPrev();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            goNext();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Right)
+            {
+                goNext();
+            }
+            if (keyData == Keys.Left)
+            {
+                goPrev();
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            String thiFile = allImages[k];
+            TryToDelete(thiFile);
+            allImages.Remove(thiFile); 
+            k++;
+            getPicture(k);
+        }
+
+        private void setAsDesktop_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("set as deskptop");
+            String thiFile = allImages[k];
+        }
+
+        private void fitToScreen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void goNext()
+        {
+            if (k < allImages.Count - 1 && allImages.Count > 0)
+            {
+                k++;
+                getPicture(k);
+            }
+            else k = 0;
+        }
+
+        void goPrev()
+        {
+            if (0 < k && allImages.Count > 0)
+            {
+                k--;
+                getPicture(k);
+            }
+            else k = allImages.Count - 1;
+        }
+
+        void getPicture(int m)
+        {
+            String nextFile = allImages[m];
+            fitImage(nextFile);
+            setFileName(nextFile);
+        }
+
+        void setFileName(String filePath)
+        {
+            String str = Path.GetFileName(filePath);
+            this.Text = "G R A M > " + str;
+        }
+
+        static bool TryToDelete(string f)
+        {
+            try
+            {
+                Console.WriteLine(f);
+                if (System.IO.File.Exists(f))
+                {
+                    File.Delete(f);  
+                    //FileSystem.DeleteFile(f);
+                    Console.WriteLine("deleted");
+                     
+                }
+                else
+                {
+                    Console.WriteLine("File does not exist.");
+                }
+
+
+                return true;
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Can't delete file");
+                return false;
+            }
+        }
+
+
+        /*
+        void SetWallpaper(String path)
+        {
+            // DLL Import
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            private static extern int SystemParametersInfo(
+            Int32 uAction,
+            Int32 uParam,
+            String lpvParam,
+            Int32 fuWinIni);
+
+            // Consts
+            private const Int32 SPI_SETDESKWALLPAPER = 20;
+            private const Int32 SPIF_UPDATEINIFILE = 0x01;
+            private const Int32 SPIF_SENDWININICHANGE = 0x02;
+
+            // Changing the background.
+
+            SystemParametersInfo(
+            SPI_SETDESKWALLPAPER,
+            0,
+            path, // desktopBackground is the bmp location
+            SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    */
+
+    }
+}
